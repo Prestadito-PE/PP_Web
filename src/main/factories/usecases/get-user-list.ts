19921d66f18e03c2aac05f13f63f9942da7dde85
jav17 <junioravila1@gmail.com>
2023-02-17 12:32:51 -0500

    change url

--- src/main/factories/usecases/get-user-list.ts
+++ src/main/factories/usecases/get-user-list.ts
@@ -4,4 +4,4 @@ import { RemoteLoadSurveyList } from '../../../../src/data/usecases'
 import { LoadSurveyList } from '@/domain/usecases'
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList =>
-  new RemoteLoadSurveyList('http://localhost:1001/api/security/users/active', makeAuthorizeHttpClientDecorator())
+  new RemoteLoadSurveyList('https://localhost:7082/api/security/users', makeAuthorizeHttpClientDecorator())
