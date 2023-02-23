8cf81ec44cc881bcdb3c4100ed4f56db49d01a73
jav17 <junioravila1@gmail.com>
2023-02-23 15:37:32 -0500

    add header Authorization / change url endpoint /security/api/users/all (api gateway)

--- src/main/decorators/authorize-http-client-decorator.ts
+++ src/main/decorators/authorize-http-client-decorator.ts
@@ -2,20 +2,25 @@ import { HttpClient, HttpRequest, HttpResponse } from '@/data/protocols/http'
 import { GetStorage } from '@/data/protocols/cache'
 
 export class AuthorizeHttpClientDecorator implements HttpClient {
-  constructor (
+  constructor(
     private readonly getStorage: GetStorage,
     private readonly httpClient: HttpClient
-  ) {}
+  ) { }
 
-  async request (data: HttpRequest): Promise<HttpResponse> {
-    const account = this.getStorage.get('account')
-    if (account?.accessToken) {
-      Object.assign(data, {
-        headers: Object.assign(data.headers || {}, {
-          'x-access-token': account.accessToken
-        })
+  async request(data: HttpRequest): Promise<HttpResponse> {
+    // const account = this.getStorage.get('account')
+    // if (account?.accessToken) {
+    //   Object.assign(data, {
+    //     headers: Object.assign(data.headers || {}, {
+    //       'x-access-token': account.accessToken
+    //     })
+    //   })
+    // }
+    Object.assign(data, {
+      headers: Object.assign(data.headers || {}, {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZjM0ODRhZmUxMTcwZjI5ZTJiMjg3MCIsInN0ckRPSSI6IiIsInN0ckNvZGVSb2wiOiJBRE0iLCJzdHJEZXNjcmlwdGlvblJvbCI6IkFkbWluaXN0cmFkb3IiLCJzdHJFbWFpbCI6Imp1bmlvcmF2aWxhMUBnbWFpbC5jb20iLCJuYmYiOjE2NzcxODI5MjcsImV4cCI6MTY3NzE4MzIyNywiaWF0IjoxNjc3MTgzMjI3LCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo1MDAxLyIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjUwMDEvIn0.qJc_MQZ4IeixXqZfh9jdnW58iNCBRSd-MH19rsu27yY'
       })
-    }
+    })
     const httpResponse = await this.httpClient.request(data)
     return httpResponse
   }
--- src/main/factories/usecases/get-user-list.ts
+++ src/main/factories/usecases/get-user-list.ts
@@ -4,4 +4,4 @@ import { RemoteLoadSurveyList } from '../../../../src/data/usecases'
 import { LoadSurveyList } from '@/domain/usecases'
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList =>
-  new RemoteLoadSurveyList('https://localhost:7082/api/security/users', makeAuthorizeHttpClientDecorator())
+  new RemoteLoadSurveyList('https://localhost:1000/security/api/users/all', makeAuthorizeHttpClientDecorator())
